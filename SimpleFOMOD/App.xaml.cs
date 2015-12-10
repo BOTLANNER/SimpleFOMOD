dbeef035a4775d8a1755a93591dd3c7c4cce2f26
Kwazzi <schilds@ironedgegroup.com>
2015-12-10 14:23:40 -0600

    Renamed errythang

--- SimpleFOMOD/App.xaml.cs
+++ SimpleFOMOD/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SimpleFOMODv3
+namespace SimpleFOMOD
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- SimpleFOMOD/MainWindow.xaml.cs
+++ SimpleFOMOD/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using MahApps.Metro;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,13 +13,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-    
-namespace SimpleFOMODv3
+using MahApps.Metro.Controls;
+
+namespace SimpleFOMOD
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
